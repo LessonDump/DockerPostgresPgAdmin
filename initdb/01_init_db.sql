4778db1c14ad14c4a836b00dbae59b9db6f9e0ab
Aleksey Voko <voko.aleksey@gmail.com>
2023-02-04 05:29:02 +0400

    feat(all): add 'docker-compose.yaml'

---
+++ initdb/01_init_db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE book
+(
+    book_id SERIAL PRIMARY KEY,
+    title   VARCHAR(50),
+    author  VARCHAR(30),
+    price   DECIMAL(8, 2),
+    amount  INT
+);
+
+INSERT INTO book (title, author, price, amount)
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
+       ('Белая гвардия', 'Булгаков М.А.', 540.50, 5),
+       ('Идиот', 'Достоевский Ф.М.', 460.00, 10),
+       ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
